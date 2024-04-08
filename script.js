dd3887e99e4d37e9e55b7bb072d48a610380d07f
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-04-08 20:47:41 +0300

    Add numbers and filtered variable

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
+  const numbers = array.map((el) => Number(el));
+  const filtered = numbers.filter();
 };
