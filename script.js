13d1363b079e243ce3b952bcd11f486d129a86e1
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-04-13 00:06:47 +0300

    Declare getMean function in calculate function

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
   const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
+  const mean = getMean(numbers);
 };
