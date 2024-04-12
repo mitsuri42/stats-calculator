21938387a6d121dd143cf0b94233393f52313b03
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-04-12 23:29:09 +0300

    Refactor getMean function

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const getMean = (array) => {
-  const sum = array.reduce((acc, el) => acc + el, 0);
-  const mean = sum / array.length;
-  return mean;
-}
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
-  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-}
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+};
