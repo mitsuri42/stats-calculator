7431fc370bc4467ddf5bab0a2a28e76b431ae1b4
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-04-15 23:13:03 +0300

    Add sort method to array in getMedian function

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length;
 
 const getMedian = (array) => {
-
-}
+  const sorted = array.sort();
+};
 
 const calculate = () => {
   const value = document.querySelector("#numbers").value;
   const array = value.split(/,\s*/g);
-  const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
-  
+  const numbers = array.map((el) => Number(el)).filter((el) => !isNaN(el));
+
   const mean = getMean(numbers);
 
   document.querySelector("#mean").textContent = mean;
-}
+};
