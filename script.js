e20a63e50b78fccab0718aaf397e03e9fadc1f6c
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-04-17 17:32:00 +0300

    Add median variable

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const getMean = (array) => array.reduce((acc, el) => acc + el, 0) / array.length
 
 const getMedian = (array) => {
   const sorted = array.sort((a, b) => a - b);
+  const median = array.length % 2 === 0 ? getMean([sorted[array.length / 2], sorted[array.length / 2 - 1]]) : sorted[Math.floor(array.length / 2)];
 };
 
 const calculate = () => {
