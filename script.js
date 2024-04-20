6c430f064a42294077b9230d4d17861df559af3f
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-04-20 19:36:47 +0300

    Add getMode function to calculate logic

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const getMode = (array) => {
   }
   const highest = Object.keys(counts).sort((a, b) => counts[b] - counts[a])[0];
   const mode = Object.keys(counts).filter((el) => counts[el] === counts[highest]);
+  return mode.join(", ");
 };
 
 const calculate = () => {
@@ -25,7 +26,9 @@ const calculate = () => {
 
   const mean = getMean(numbers);
   const median = getMedian(numbers);
+  const mode = getMode(numbers);
 
+  document.querySelector("#mode").textContent = mode;
   document.querySelector("#mean").textContent = mean;
   document.querySelector("#median").textContent = median;
 };
