b7c72a237da44b7e2e1717f42288fda1cf352f66
Dmitriy Pysarchuk <dmit.pysarchuk@gmail.com>
2024-04-23 16:45:00 +0300

    Add squaredDifferences variable

--- script.js
+++ script.js
@@ -26,6 +26,7 @@ const getRange = (array) => {
 const getVariance = (array) => {
   const mean = getMean(array);
   const differences = array.map((el) => el - mean);
+  const squaredDifferences = differences.map((el) => el ** 2);
 };
 
 const calculate = () => {
